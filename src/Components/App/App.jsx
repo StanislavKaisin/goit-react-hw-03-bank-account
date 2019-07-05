bbafebced97db538a63dd21ca4a3fe0060fd9f96
Stanislav Kaisin <8050361@gmail.com>
2019-07-05 22:19:07 +0300

    fixed reading from ls

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Dashboard from '../bank-account/Dashboard/Dashboard';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
