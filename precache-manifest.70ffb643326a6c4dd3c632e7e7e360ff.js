e18442e876d27ce64391e61ee49be895e723946a
Stanislav Kaisin <8050361@gmail.com>
2019-07-05 22:25:27 +0300

    Updates

---
+++ precache-manifest.70ffb643326a6c4dd3c632e7e7e360ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8309a62e375a3af9892b75cb4ee93131",
+    "url": "/goit-react-hw-03-bank-account/index.html"
+  },
+  {
+    "revision": "9cc05f985a70fb4057a7",
+    "url": "/goit-react-hw-03-bank-account/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "6ddd5ba623eccd2c26df",
+    "url": "/goit-react-hw-03-bank-account/static/css/main.e25e7cf0.chunk.css"
+  },
+  {
+    "revision": "9cc05f985a70fb4057a7",
+    "url": "/goit-react-hw-03-bank-account/static/js/2.d5c99b9e.chunk.js"
+  },
+  {
+    "revision": "6ddd5ba623eccd2c26df",
+    "url": "/goit-react-hw-03-bank-account/static/js/main.3436a6e4.chunk.js"
+  },
+  {
+    "revision": "1d8f9ed0b38c93c66413",
+    "url": "/goit-react-hw-03-bank-account/static/js/runtime~main.d734538f.js"
+  }
+]);
